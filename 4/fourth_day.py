4a7a1032dd34c218f6ea0e192b03fbf5fd0a9553
CinqueIzumi <nofeednolife@gmail.com>
2022-12-07 17:02:23 +0100

    Add day 4 pt. 2

--- 4/fourth_day.py
+++ 4/fourth_day.py
@@ -27,4 +27,37 @@ def getAnswerPartOne():
 
     print(f'overlaps: {total}')
 
-getAnswerPartOne()
+def getAnswerPartTwo():
+    f = open("4/input_part_1.txt")
+    lines = f.readlines()
+
+    total = 0
+
+    for line in lines:
+        # Split the values in an in array
+        values = [int(number) for number in re.findall(r'\d+', line)]
+
+        # Assign the int array's values to their respective variables
+        firstStart = values[0]
+        firstEnd = values[1]
+        secondStart = values[2]
+        secondEnd = values[3]
+
+        # # Check if the second elf's areas are contained within the first elf's areas
+        # if firstStart <= secondStart and firstEnd >= secondEnd:
+        #     total += 1
+        # # Check if the first elf's areas are contained within the second elf's areas
+        # elif secondStart <= firstStart and secondEnd >= firstEnd:
+        #     total += 1
+
+        if (firstStart <= secondStart) and (firstEnd >= secondStart):
+            total += 1
+        elif (secondStart <= firstStart) and (secondEnd >= firstStart):
+            total += 1
+
+    f.close()
+
+    print(f'overlaps in total: {total}')
+
+# getAnswerPartOne()
+getAnswerPartTwo()
